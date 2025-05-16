e628ad866c9ac63ea6d564fb94b306e620ff1130
Kilari Teja <kilariteja9@gmail.com>
2025-05-16 22:26:31 +0530

    Remove some examples

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 mod macros;
 
-fn sum(a: u32, b: u32) -> u64 {
-    sprinkle_magic_here!();
-}
-
 fn is_even(a: u32) -> bool {
-    sprinkle_magic_here!();
-}
-
-fn string_reverse(string: String) -> String {
     sprinkle_magic_here!();
 }
 
 fn main() {
     println!("is_even {:?}", is_even(3));
-    println!("sum {:?}", sum(1, 3));
-    println!("string_reverse {:?}", string_reverse("Hello World".to_owned()));
 }
