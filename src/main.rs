9397b0ba2e81689aecf2cba5ba3d20582ec6c668
Kilari Teja <kilariteja9@gmail.com>
2025-05-19 11:03:12 +0530

    Add a couple more demos

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod macros;
 
 fn is_even(a: u32) -> bool {
-    todo!();
+    todo_magic!();
+}
+
+fn reverse_a_string(a: &str) -> String {
+    todo_magic!();
+}
+
+fn pluralize(count: usize, a: &str) -> String {
+    todo_magic!();
 }
 
 fn main() {
     println!("is_even {:?}", is_even(3));
+    println!("reverse seting {:?}", reverse_a_string("Hello World"));
+    println!("reverse seting {:?}", pluralize(0, "ant"));
 }
